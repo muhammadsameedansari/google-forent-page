56a5c98320ab35cff4e8117785902d7d014e1861
sameed-ansari <mysoftwerehousedreem@gmail.com>
2024-05-01 00:07:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const editableText= document.getElementById('editable-text');
+
+if(localStorage.getItem('savedText')){
+  editableText.textContent=localStorage.getItem('savedText');
+}
+
+
+editableText.addEventListener('input', function(){
+
+  localStorage.setItem('savedText',this.textContent);
+})
+
